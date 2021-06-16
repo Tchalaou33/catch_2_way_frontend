4bd037b9a9e33f63114e7ba9c29a0cfa272633e6
Tchalaou33 <attchalaou@gmail.com>
2021-06-16 14:33:03 -0400

    added function to get request

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("in index.js");
+// if I want to reuse code for fetch
+const endPoint = "http://localhost:3000/api/v1/ways"
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    getWays()
+})
+function getWays() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(ways => {
+        console.log(ways);
+})
+}
