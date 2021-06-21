78248cf01b63f1bf5857151f9c3a7e519c1c6ee5
Tchalaou33 <attchalaou@gmail.com>
2021-06-21 03:59:54 -0400

    added prevent defualt values

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const endPoint = "http://localhost:3000/api/v1/ways"
 
 document.addEventListener('DOMContentLoaded', () => {
     getWays()
+
+    const createWayForm = document.querySelector("#create-way-form")
+
+    createWayForm.addEventListener("submit", (e) => createFormHandler(e))
 })
+
 function getWays() {
     fetch(endPoint)
     .then(response => response.json())
@@ -22,4 +27,17 @@ function getWays() {
       })
 
     })
+}
+
+
+function createFormHandler(e) {
+    e.preventDefault()
+    const colorInput = document.querySelector('#input-color').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const destinationId = parseInt(document.querySelector('#destinations').value)
+    postFetch(colorInput, descriptionInput, destinationId)
+}
+
+function postFetch(color, description, destination_id) {
+    console.log(color, description, destination_id);
 }
